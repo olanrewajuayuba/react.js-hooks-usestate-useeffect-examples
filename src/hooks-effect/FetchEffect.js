9ee42c50f9d031fd5c57338ac5d5b6c774c46894
olanrewajuayuba <olanrewajayuba@gmail.com>
2019-12-13 23:51:17 +0100

    first commit

---
+++ src/hooks-effect/FetchEffect.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import '../App.css';
+
+function FetchEffect() {
+
+    const [product, setProduct] = useState(null);
+    const [url, setState] = useState(`http://localhost:3000/api/v1/products`);
+
+    useEffect(() => {
+
+        let abortController = new AbortController();
+
+        const loadProduct = async () => {
+            try {
+                const response = await fetch(url, { signal: abortController.signal });
+                const data = await response.json();
+                console.log(`Data fetched: ${data}`);
+                setProduct(data);
+            } catch (error) {
+                console.log(`Error fetch data: ${error}`);
+                throw error;
+            }
+        };
+
+        loadProduct();
+
+        return () => {
+            console.log("Cancel fetch: abort");
+            abortController.abort();
+        };
+    }, [url]);
+
+    if (!product) {
+        return <div className="App">Loading...</div>;
+    }
+
+    return (
+        <div className="App">
+            {product.map((item, idx) => (
+                <p key={idx}>{item.prod_name}</p>
+            ))}
+        </div>
+    );
+}
+
+export default FetchEffect;
